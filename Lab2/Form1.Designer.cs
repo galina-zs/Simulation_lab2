f59f5f89b51f52bc4353bf7f97d96ad5cdb72c3d
galina-zs <zab.galina.24@gmail.com>
2021-02-18 21:14:28 +0300

    All functions (time indicator, pause control, scaling)

--- Lab2/Form1.Designer.cs
+++ Lab2/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.heightLabel = new System.Windows.Forms.Label();
             this.angleLabel = new System.Windows.Forms.Label();
@@ -41,6 +41,9 @@
             this.speedNumericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.launchButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.continueButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.angleNumericUpDown1)).BeginInit();
@@ -49,22 +52,22 @@
             // 
             // chart1
             // 
-            chartArea2.AxisX.Maximum = 100D;
-            chartArea2.AxisX.Minimum = 0D;
-            chartArea2.AxisY.Maximum = 100D;
-            chartArea2.AxisY.Minimum = 0D;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            chartArea1.AxisX.Maximum = 100D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(0, 150);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(800, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -140,7 +143,8 @@
             // 
             // launchButton
             // 
-            this.launchButton.Location = new System.Drawing.Point(256, 93);
+            this.launchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.launchButton.Location = new System.Drawing.Point(255, 22);
             this.launchButton.Name = "launchButton";
             this.launchButton.Size = new System.Drawing.Size(75, 23);
             this.launchButton.TabIndex = 7;
@@ -152,11 +156,46 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timerLabel.Location = new System.Drawing.Point(281, 96);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(49, 20);
+            this.timerLabel.TabIndex = 8;
+            this.timerLabel.Text = "00:00";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pauseButton.Location = new System.Drawing.Point(344, 58);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 9;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // continueButton
+            // 
+            this.continueButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.continueButton.Location = new System.Drawing.Point(255, 58);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(75, 23);
+            this.continueButton.TabIndex = 10;
+            this.continueButton.Text = "Continue";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.timerLabel);
             this.Controls.Add(this.launchButton);
             this.Controls.Add(this.speedNumericUpDown2);
             this.Controls.Add(this.angleNumericUpDown1);
@@ -187,6 +226,9 @@
         private System.Windows.Forms.NumericUpDown speedNumericUpDown2;
         private System.Windows.Forms.Button launchButton;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button continueButton;
     }
 }
 
--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -13,14 +13,16 @@ namespace Lab2
     public partial class Form1 : Form
     {
         decimal t = 0, x0, y0, v0, cosa, sina;
+        double maxX, maxY;
+        int time_after_start = 0;
 
         const decimal g = 9.81M;
         const decimal dt = 0.1M;
         public Form1()
         {
             InitializeComponent();
-            chart1.ChartAreas[0].AxisX.Maximum = 20;
-            chart1.ChartAreas[0].AxisY.Maximum = 20;
+            pauseButton.Enabled = false;
+            continueButton.Enabled = false;
         }
 
         private void launchButton_Click(object sender, EventArgs e)
@@ -37,26 +39,56 @@ namespace Lab2
                 sina = (decimal)Math.Sin(a);
                 chart1.Series[0].Points.AddXY(x0, y0);
                 timer1.Start();
+                pauseButton.Enabled = true;
+                continueButton.Enabled = false;
+                FindMaximum();
+                chart1.ChartAreas[0].AxisX.Maximum = maxX;
+                chart1.ChartAreas[0].AxisY.Maximum = maxY;
+                time_after_start = 0;
             }
+        }
+
+        private void FindMaximum()
+        {
+            double time = ((double)(v0 * sina) + Math.Sqrt((double)(v0 * v0 * sina * sina) + (double)(2 * g * heightNumericUpDown1.Value))) / (double)g;
+            maxX = (double)(v0 * cosa) * time;
+            maxX = Math.Ceiling(maxX) + 2;
+            maxY = (double)heightNumericUpDown1.Value + (double)(v0 * v0 * sina * sina) / (double)(2 * g);
+            maxY = Math.Ceiling(maxY) + 2;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pauseButton.Enabled = false;
+            continueButton.Enabled = true;
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            pauseButton.Enabled = true;
+            continueButton.Enabled = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             t += dt;
+            time_after_start += timer1.Interval;
+            if (time_after_start % 1000 == 0)
+            {
+                var timespan = TimeSpan.FromSeconds(time_after_start / 1000);
+                timerLabel.Text = timespan.ToString(@"mm\:ss");
+            }
             decimal x = x0 + v0 * cosa * t;
             decimal y = y0 + v0 * sina * t - g * t * t / 2;
-            if ((x >= (decimal)chart1.ChartAreas[0].AxisX.Maximum - 20) && Math.Ceiling(x) % 5 == 0)
-            {
-                chart1.ChartAreas[0].AxisX.Maximum = (double)Math.Ceiling(x) + 10;
-            }
-            if ((y >= (decimal)chart1.ChartAreas[0].AxisY.Maximum - 20) 
-                && y > (decimal)chart1.ChartAreas[0].AxisY.Maximum
-                && Math.Ceiling(y) % 5 == 0)
-            {
-                chart1.ChartAreas[0].AxisY.Maximum = (double)Math.Ceiling(y) + 10;
-            }
             chart1.Series[0].Points.AddXY(x, y);
-            if (y <= 0) timer1.Stop();
+            if (y <= 0)
+            {
+                timer1.Stop();
+                pauseButton.Enabled = false;
+                continueButton.Enabled = false;
+            }
         }
     }
 }
